9802a0919e7e36fae2d78fcce60c19a38d02e057
tender_bum <brickmen75@gmail.com>
2019-02-21 01:55:41 +0300

    Create execution tree nodes along with sync_execute and async_execute functions

--- task_runner/task_runner.h
+++ task_runner/task_runner.h
@@ -4,6 +4,7 @@
 #include <future>
 #include <variant>
 
+#include "event.h"
 #include "task_system.h"
 
 namespace base {
@@ -15,14 +16,249 @@ template <class InvokeContext>
 void cast_invoke(void* p) {
     std::unique_ptr<InvokeContext> ctx{static_cast<InvokeContext*>(p)};
     try {
-        std::get<0>(*ctx).set_value(std::apply(std::move(std::get<1>(*ctx)),
-                                               std::move(std::get<2>(*ctx))));
+        if constexpr (std::is_void_v<decltype(
+                          std::apply(std::move(std::get<1>(*ctx)),
+                                     std::move(std::get<2>(*ctx))))>) {
+            std::apply(std::move(std::get<1>(*ctx)),
+                       std::move(std::get<2>(*ctx)));
+            std::get<0>(*ctx).set_value();
+        } else {
+            std::get<0>(*ctx).set_value(std::apply(
+                std::move(std::get<1>(*ctx)), std::move(std::get<2>(*ctx))));
+        }
     } catch (...) {
         std::get<0>(*ctx).set_exception(std::current_exception());
     }
 }
 
+// InvokeContext == std::tuple<F, std::tuple<Args...>>
+template <class InvokeContext>
+void cast_invoke_detached(void* p) {
+    std::unique_ptr<InvokeContext> ctx{static_cast<InvokeContext*>(p)};
+    std::apply(std::move(std::get<0>(*ctx)), std::move(std::get<1>(*ctx)));
+}
+
 }  // namespace detail
+
+struct nothing {};
+
+constexpr nothing nothing_v;
+
+template <class T>
+struct wrap_into_nothing {
+    using type = T;
+};
+
+template <>
+struct wrap_into_nothing<void> {
+    using type = nothing;
+};
+
+template <class T>
+using wrap_into_nothing_t = typename wrap_into_nothing<T>::type;
+
+namespace exec_nodes {
+
+template <class F>
+struct leaf : F {
+    template <class... Args>
+    using invoke_result_t =
+        wrap_into_nothing_t<std::invoke_result_t<F, Args...>>;
+
+    leaf(F f) : F{std::move(f)} {}
+
+    template <class TaskRunner, class Then, class... Args>
+    void execute(TaskRunner* const tr, Then then, Args... args) & {
+        assert(tr != nullptr);
+        DBG_OUT("execute leaf tid: " << std::this_thread::get_id());
+        if constexpr (std::is_void_v<std::invoke_result_t<F, Args...>>) {
+            (*this)(std::move(args)...);
+            std::move(then)(nothing_v);
+        } else {
+            std::move(then)((*this)(std::move(args)...));
+        }
+    }
+
+    template <class Then>
+    auto then(Then f);
+};
+
+template <class F>
+auto make_leaf(F f) {
+    return leaf{std::move(f)};
+}
+
+template <class A, class B>
+struct seq : A, B {
+    template <class... Args>
+    using invoke_result_t = typename B::template invoke_result_t<
+        typename A::template invoke_result_t<Args...>>;
+
+    seq(A&& a, B&& b) : A{std::move(a)}, B{std::move(b)} {}
+
+    template <class TaskRunner, class Then, class... Args>
+    void execute(TaskRunner* const tr, Then then, Args... args) & {
+        assert(tr != nullptr);
+        DBG_OUT("execute seq tid: " << std::this_thread::get_id());
+        A::execute(tr,
+                   [tr, f = static_cast<B*>(this),
+                    then = std::move(then)](auto... a_args) mutable {
+                       f->execute(tr, std::move(then), std::move(a_args)...);
+                   },
+                   std::move(args)...);
+    }
+
+    template <class Then>
+    auto then(Then f);
+};
+
+template <class A, class B>
+auto make_seq(A a, B b) {
+    return seq{std::move(a), std::move(b)};
+}
+
+template <class... Fs>
+struct all : Fs... {
+    template <class... Args>
+    using invoke_result_t =
+        std::tuple<typename Fs::template invoke_result_t<Args...>...>;
+
+    all(Fs... fs) : Fs{std::move(fs)}... {}
+
+    template <class TaskRunner, class Then, class... Args>
+    void execute(TaskRunner* const tr, Then then, Args... args) & {
+        assert(tr != nullptr);
+        DBG_OUT("execute all tid: " << std::this_thread::get_id());
+        execute_impl(tr, std::move(then), std::index_sequence_for<Fs...>{},
+                     std::move(args)...);
+    }
+
+    template <class TaskRunner, class Then, std::size_t... Is, class... Args>
+    void execute_impl(TaskRunner& tr, Then then, std::index_sequence<Is...>,
+                      Args... args) {
+        struct state_type {
+            std::atomic<int> left;
+            Then then;
+            std::tuple<typename Fs::template invoke_result_t<Args...>...>
+                result;
+        };
+        auto* state = new state_type{sizeof...(Fs), std::move(then), {}};
+
+        (..., tr->run(
+                  [tr, state, f = static_cast<Fs*>(this)](auto... args) {
+                      f->execute(tr,
+                                 [state](auto&& result) {
+                                     std::get<Is>(state->result) =
+                                         std::forward<decltype(result)>(result);
+                                     if (state->left.fetch_sub(1) == 1) {
+                                         DBG_OUT("all's then call tid: "
+                                                 << std::this_thread::get_id());
+                                         state->then(std::move(state->result));
+                                         delete state;
+                                     }
+                                 },
+                                 std::move(args)...);
+                  },
+                  args...));
+    }
+
+    template <class Then>
+    auto then(Then f);
+};
+
+template <class... Fs>
+auto make_all(Fs... fs) {
+    return all{std::move(fs)...};
+}
+
+template <class T, class = void>
+struct is_exec_node : std::false_type {};
+
+template <class F>
+struct is_exec_node<leaf<F>> : std::true_type {};
+
+template <class A, class B>
+struct is_exec_node<seq<A, B>> : std::true_type {};
+
+template <class... Fs>
+struct is_exec_node<all<Fs...>> : std::true_type {};
+
+template <class T>
+constexpr bool is_exec_node_v = is_exec_node<T>::value;
+
+template <class F>
+auto try_transform(F f) {
+    if constexpr (is_exec_node_v<std::decay_t<F>>) {
+        return f;
+    } else {
+        return make_leaf(std::move(f));
+    }
+};
+
+template <class F>
+template <class Then>
+auto leaf<F>::then(Then f) {
+    return make_seq(std::move(*this), try_transform(std::move(f)));
+}
+
+template <class A, class B>
+template <class Then>
+auto seq<A, B>::then(Then f) {
+    return make_seq(std::move(*this), try_transform(std::move(f)));
+}
+
+template <class... Fs>
+template <class Then>
+auto all<Fs...>::then(Then f) {
+    return make_seq(std::move(*this), try_transform(std::move(f)));
+}
+
+template <class... Fs>
+auto when_all(Fs... fs) {
+    return make_all(try_transform(std::move(fs))...);
+}
+
+template <class TaskRunner, class ExecTree, class Then, class... Args>
+void sync_execute(TaskRunner* tr, ExecTree* tree, Then then, Args... args) {
+    assert(tr != nullptr);
+    assert(tree != nullptr);
+    base::manual_event finish_event;
+    tree->execute(tr,
+                  [&finish_event, &then](auto... args) {
+                      then(std::move(args)...);
+                      finish_event.notify();
+                  },
+                  std::move(args)...);
+    DBG_OUT("waiting tid: " << std::this_thread::get_id());
+    finish_event.wait();
+}
+
+template <class ExecTree, class... Args>
+using exec_tree_invoke_result_t = typename ExecTree::template invoke_result_t<Args...>;
+
+template <class TaskRunner, class ExecTree, class... Args>
+[[nodiscard]] std::future<
+    exec_tree_invoke_result_t<ExecTree, std::decay_t<Args>...>>
+async_execute(TaskRunner* tr, ExecTree* tree, Args... args) {
+    assert(tr != nullptr);
+    assert(tree != nullptr);
+    std::promise<exec_tree_invoke_result_t<ExecTree, std::decay_t<Args>...>>
+        promise;
+    auto future = promise.get_future();
+    tr->run(
+        [tr, tree, promise = std::move(promise)](auto... args) mutable {
+            tree->execute(
+                tr,
+                [promise = std::move(promise)](auto... result) mutable {
+                    promise.set_value(std::move(result)...);
+                },
+                std::move(args)...);
+        },
+        std::move(args)...);
+    return future;
+}
+
+}  // namespace exec_nodes
 
 template <class TaskSystem>
 class task_runner_base {
@@ -34,7 +270,8 @@ class task_runner_base {
         using result_t =
             std::invoke_result_t<std::decay_t<F>, std::decay_t<Args>...>;
         using args_tuple = std::tuple<std::decay_t<Args>...>;
-        using invoke_tuple = std::tuple<std::promise<result_t>, std::decay_t<F>, args_tuple>;
+        using invoke_tuple =
+            std::tuple<std::promise<result_t>, std::decay_t<F>, args_tuple>;
 
         auto p = std::make_unique<invoke_tuple>(
             std::promise<result_t>{}, std::forward<F>(f),
@@ -47,6 +284,20 @@ class task_runner_base {
         p.release();
 
         return ret;
+    }
+
+    template <class F, class... Args>
+    void run(F&& f, Args&&... args) {
+        using args_tuple = std::tuple<std::decay_t<Args>...>;
+        using invoke_tuple = std::tuple<std::decay_t<F>, args_tuple>;
+
+        auto p = std::make_unique<invoke_tuple>(
+            std::forward<F>(f),
+            std::forward_as_tuple(std::forward<Args>(args)...));
+
+        task_system_.run(detail::cast_invoke_detached<invoke_tuple>, p.get());
+        // Context transfered to task system successfully.
+        p.release();
     }
 
   private:
--- task_runner/ut/task_runner_ut.cpp
+++ task_runner/ut/task_runner_ut.cpp
@@ -1,9 +1,10 @@
 #include "task_runner/task_runner.h"
 
-#include <iostream>
+#include <numeric>
+#include <set>
 
-#include "task_runner/event.h"
 #include "catch/catch.h"
+#include "task_runner/event.h"
 
 namespace {
 
@@ -50,4 +51,167 @@ TEST_CASE("task_runner_concurrency_test", "[task_runner]") {
     REQUIRE(fut1.get());
     REQUIRE(fut2.get());
     REQUIRE(std::adjacent_find(vals.begin(), vals.end()) == vals.end());
+}
+
+TEST_CASE("exec_nodes_leaf_test", "[exec_nodes]") {
+    std::vector<int> v;
+    base::simple_task_runner tr;
+    base::exec_nodes::leaf node{[&] { v.push_back(1); }};
+
+    base::exec_nodes::sync_execute(&tr, &node,
+                                   [&](base::nothing) { v.push_back(2); });
+    REQUIRE(std::is_sorted(v.begin(), v.end()));
+}
+
+TEST_CASE("exec_nodes_seq_test", "[exec_nodes]") {
+    std::vector<int> v;
+    base::simple_task_runner tr;
+    auto node = base::exec_nodes::leaf{[&] { v.push_back(1); }}.then(
+        [&](base::nothing) { v.push_back(2); });
+
+    base::exec_nodes::sync_execute(&tr, &node,
+                                   [&](base::nothing) { v.push_back(3); });
+    REQUIRE(std::is_sorted(v.begin(), v.end()));
+}
+
+TEST_CASE("exec_nodes_all_test", "[exec_nodes]") {
+    std::vector<int> v;
+    base::simple_task_runner tr;
+    std::mutex mtx;
+    auto node = base::exec_nodes::when_all(
+        [&] {
+            std::unique_lock lck{mtx};
+            v.push_back(1);
+        },
+        [&] {
+            std::unique_lock lck{mtx};
+            v.push_back(2);
+        },
+        [&] {
+            std::unique_lock lck{mtx};
+            v.push_back(3);
+        },
+        [&] {
+            std::unique_lock lck{mtx};
+            v.push_back(4);
+        });
+
+    base::exec_nodes::sync_execute(&tr, &node, [&](auto) { v.push_back(5); });
+    std::vector<int> eta(5);
+    std::iota(eta.begin(), eta.end(), 1);
+    std::sort(v.begin(), v.end());
+    REQUIRE(std::equal(v.begin(), v.end(), eta.begin()));
+}
+
+TEST_CASE("exec_nodes_all_big_test", "[exec_nodes]") {
+    DBG_OUT("exec_nodes_all_big_test start");
+    std::vector<int> v;
+    base::simple_task_runner tr;
+    std::mutex mtx;
+
+    auto node = base::exec_nodes::when_all(
+                    [&] {
+                        std::unique_lock lck{mtx};
+                        v.push_back(0);
+                    },
+                    [&] {
+                        std::unique_lock lck{mtx};
+                        v.push_back(0);
+                    },
+                    [&] {
+                        std::unique_lock lck{mtx};
+                        v.push_back(0);
+                    },
+                    [&] {
+                        std::unique_lock lck{mtx};
+                        v.push_back(0);
+                    })
+                    .then([&](auto) { v.push_back(1); })
+                    .then(base::exec_nodes::when_all(
+                        [&](base::nothing) {
+                            std::unique_lock lck{mtx};
+                            v.push_back(0);
+                        },
+                        [&](base::nothing) {
+                            std::unique_lock lck{mtx};
+                            v.push_back(0);
+                        },
+                        [&](base::nothing) {
+                            std::unique_lock lck{mtx};
+                            v.push_back(0);
+                        },
+                        [&](base::nothing) {
+                            std::unique_lock lck{mtx};
+                            v.push_back(0);
+                        }))
+                    .then([&](auto) { v.push_back(1); });
+
+    base::exec_nodes::sync_execute(&tr, &node, [](base::nothing) {});
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    REQUIRE(std::count(v.begin(), v.end(), 1) == 2);
+    DBG_OUT("exec_nodes_all_big_test finish");
+}
+
+TEST_CASE("exec_nodes_eval_test", "[exec_nodes]") {
+    base::simple_task_runner tr;
+
+    auto node =
+        base::exec_nodes::all{
+            base::exec_nodes::leaf{[] { return 1; }},
+            base::exec_nodes::leaf{[] { return 2; }},
+            base::exec_nodes::leaf{[] { return 3; }},
+            base::exec_nodes::leaf{[] { return 4; }},
+            base::exec_nodes::leaf{[] { return 5; }},
+        }
+            .then([](auto res) {
+                return std::apply([](auto... vals) { return (... + vals); },
+                                  res);
+            });
+
+    int result;
+    base::exec_nodes::sync_execute(&tr, &node,
+                                   [&result](int val) { result = val; });
+    REQUIRE(result == 1 + 2 + 3 + 4 + 5);
+}
+
+TEST_CASE("exec_nodes_eval2_test", "[exec_nodes]") {
+    base::simple_task_runner tr;
+
+    auto simple_func = [](int val) {
+        return val * 2 + val + 4 + val - 3 + val / 2 + val + 3;
+    };
+
+    auto node = base::exec_nodes::when_all([](int val) { return val * 2; },
+                                           [](int val) { return val + 4; },
+                                           [](int val) { return val - 3; },
+                                           [](int val) { return val / 2; },
+                                           [](int val) { return val + 3; })
+                    .then([](auto res) {
+                        return std::apply(
+                            [](auto... vals) { return (... + vals); }, res);
+                    });
+
+    int result;
+    auto setter = [&result](int val) { result = val; };
+
+    base::exec_nodes::sync_execute(&tr, &node, setter, 5);
+    REQUIRE(result == simple_func(5));
+
+    base::exec_nodes::sync_execute(&tr, &node, setter, 25);
+    REQUIRE(result == simple_func(25));
+
+    base::exec_nodes::sync_execute(&tr, &node, setter, 0);
+    REQUIRE(result == simple_func(0));
+}
+
+TEST_CASE("exec_nodes_async_simple", "[exec_nodes]") {
+    base::simple_task_runner tr;
+
+    auto node = base::exec_nodes::leaf{[](int a) { return a * 5; }}.then(
+        [](int b) { return b + 2; });
+
+    auto future = base::exec_nodes::async_execute(&tr, &node, 5);
+    REQUIRE(future.valid());
+    REQUIRE(future.get() == 5 * 5 + 2);
+    REQUIRE(!future.valid());
 }
